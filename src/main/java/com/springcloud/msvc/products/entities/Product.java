a86ea4feb33306a77ec1dec182ec8c9c47f8424c
Marcos Aguilar Lozano <marcosaguilar2001mal@gmail.com>
2025-05-09 18:33:23 +0200

    base del proyecto creado

---
+++ src/main/java/com/springcloud/msvc/products/entities/Product.java
@@ -0,0 +1,51 @@
+package com.springcloud.msvc.products.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    @Column(name = "create_at")
+    private LocalDate crateAt;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public LocalDate getCrateAt() {
+        return crateAt;
+    }
+    public void setCrateAt(LocalDate crateAt) {
+        this.crateAt = crateAt;
+    }
+
+    
+
+}
