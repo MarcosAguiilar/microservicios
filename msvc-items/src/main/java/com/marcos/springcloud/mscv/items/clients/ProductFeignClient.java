d86be4f8a264d5ae12b5fb524fe9e71e273a8c18
Marcos Aguilar Lozano <marcosaguilar2001mal@gmail.com>
2025-08-02 15:23:33 +0200

    Balanceo de carga añadido

--- msvc-items/src/main/java/com/marcos/springcloud/mscv/items/clients/ProductFeignClient.java
+++ msvc-items/src/main/java/com/marcos/springcloud/mscv/items/clients/ProductFeignClient.java
@@ -1,3 +1,20 @@
-public class ProductFeignClient {
-    
+package com.marcos.springcloud.mscv.items.clients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.andres.springcloud.msvc.items.models.Product;
+
+@FeignClient(name = "msvc-products")
+public interface ProductFeignClient {
+
+    @GetMapping
+    List<Product> findAll();
+
+    @GetMapping("/{id}")
+    Product details(@PathVariable Long id);
+
 }
